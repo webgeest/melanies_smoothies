dbde1783292dfd2f7468609803728ca6ce56ba61
webgeest <42408472+webgeest@users.noreply.github.com>
2025-04-29 15:27:29 +0200

    Update streamlit_app.py
    kkmn

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,8 @@ name_on_order = st.text_input('Name on Smoothie:')
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-#session = get_active_session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect(
 'Choose up to 5 ingredients:'
@@ -30,11 +28,9 @@ if ingredients_list:
         sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         ingredients_string += fruit_chosen + ' '
-    #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """'
                    ,'"""+name_on_order+ """')"""
-    #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
